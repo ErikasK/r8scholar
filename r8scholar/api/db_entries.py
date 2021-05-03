4140032d4fa55957374a81c1ea8b1fba9c98fdcc
Feesh404 <erikasklimusinas1@gmail.com>
2021-05-03 04:19:21 +0200

    some stuff

--- r8scholar/api/db_entries.py
+++ r8scholar/api/db_entries.py
@@ -5,7 +5,7 @@
 # 1. cd into /r8scholar (directory with manage.py)
 # 2. run the command --> python3 manage.py shell (this runs a python shell in our django environment)
 # 3. run this command the execute the script exec(open("./api/db_entries.py").read()) 
-from api.models import Instructor, Course, Department, Tags
+from api.models import CustomUser, Instructor, Course, Department, Tags
 
 
 class ModelGenerator:
@@ -29,6 +29,8 @@ class ModelGenerator:
             model = Course(name=data[0], department=my_department, rating=0, course_full_name=data[2])
         elif self.type == "Tag":
             model = Tags(description=data[0],subject = data[1].strip())
+        elif self.type == "Admin":
+            model = CustomUser(email=data[0], nickname=data[1], password=data[2], is_admin=data[3], is_verified=data[4].strip())
         print("Creating", model)
         model.save()
     
@@ -47,8 +49,9 @@ class ModelGenerator:
 
 
 
-if __name__ == "builtins": 
-    ModelGenerator('./api/data/departments.txt', "Department") #make departments first cause dependencies
-    ModelGenerator('./api/data/instructors.txt', "Instructor") #make instructor next 
-    ModelGenerator('./api/data/courses.txt', "Course") #courses need departments and instructors 
-    ModelGenerator('./api/data/tags.txt', "Tag")
+#if __name__ == "builtins": 
+#ModelGenerator('./api/data/departments.txt', "Department") #make departments first cause dependencies
+#ModelGenerator('./api/data/instructors.txt', "Instructor") #make instructor next 
+#ModelGenerator('./api/data/courses.txt', "Course") #courses need departments and instructors 
+#ModelGenerator('./api/data/tags.txt', "Tag")
+ModelGenerator('./api/data/admin.txt', "Admin")
